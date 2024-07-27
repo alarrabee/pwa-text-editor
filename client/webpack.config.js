9164819c3aff890da2934b34b4f2e4f8d10d761c
alarrabee <alexlarrabee10@gmail.com>
2024-07-27 15:47:42 -0500

    fix where src-sw.js file could not be located

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = () => {
 			template: './index.html',
 		}),
         new InjectManifest({
-            swSrc: './src/sw.js',
+            swSrc: '/src-sw.js',
             swDest: 'service-worker.js',
         }),
         new WebpackPwaManifest({
@@ -36,7 +36,7 @@ module.exports = () => {
             publicPath: './',
             icons: [
               {
-                src: path.resolve('assets/images/logo.png'),
+                src: path.resolve('src/images/logo.png'),
                 sizes: [96, 128, 192, 256, 384, 512],
                 destination: path.join('assets', 'icons'),
               },
